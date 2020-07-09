cddf922fdbc08fb5858b3daa1dc6175db6de328b
McCall <1059511459@qq.com>
2020-07-09 10:31:00 +0800

    制作全局Nav组件，View：404页面及路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import Home from '../views/Home.vue';
 import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
+import NotFund from '@/views/NotFund.vue';
 
 Vue.use(VueRouter);
 
@@ -24,6 +25,10 @@ const routes = [
   {
     path: '/statistics',
     component: Statistics
+  },
+  {
+    path: '*',
+    component: NotFund
   }
 
 ];
---
+++ src/views/NotFund.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div>404页面：当前页面不存在，请重新输入路径！</div>
+        <div>
+            <router-link to="/">返回首页</router-link>
+            <a href="#/">返回首页</a>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'NotFund'
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
