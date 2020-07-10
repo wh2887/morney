f9c46c4fafed9d03607ae9092469093dec96ea59
McCall <1059511459@qq.com>
2020-07-10 10:44:02 +0800

    添加nav样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,21 +1,20 @@
 <template>
-    <div class="nav">
-        <hr/>
-        <router-link to="/money">
+    <nav>
+        <router-link class="item" to="/money">
             <Icon name="money"/>
             记账
         </router-link>
-        |
-        <router-link to="/labels">
+
+        <router-link class="item" to="/labels">
             <Icon name="labels"/>
             标签
         </router-link>
-        |
-        <router-link to="/statistics">
+
+        <router-link class="item" to="/statistics">
             <Icon name="statistics"/>
             统计
         </router-link>
-    </div>
+    </nav>
 </template>
 
 <script lang="ts">
@@ -26,7 +25,24 @@
 </script>
 
 <style lang="scss" scoped>
-    .nav {
-        border: 1px solid red;
+
+    nav {
+        display: flex;
+        box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+        flex-direction: row;
+        font-size: 12px;
+
+        > .item {
+            padding: 2px 0;
+            width: 33.3333%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            .icon {
+                width: 32px;
+                height: 32px;
+            }
+        }
     }
 </style>
