411e042f3863fa9e6c47b82eb7c387e1b047dcce
McCall <1059511459@qq.com>
2020-07-11 10:50:52 +0800

    样式基本完成，开始考虑上下空隙问题

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -55,6 +55,8 @@
 
     .numberPad {
         .output {
+            @extend %clearFix;
+            @extend %innerShadow;
             font-size: 36px;
             font-family: Consolas monospace;
             padding: 9px 16px;
@@ -62,18 +64,54 @@
         }
 
         .buttons {
+            @extend %clearFix;
+
             > button {
                 width: 25%;
                 height: 64px;
                 float: left;
+                background: transparent;
+                border: none;
 
                 &.ok {
                     height: 64*2px;
                     float: right;
                 }
-                &.zero{
+
+                &.zero {
                     width: 25*2%;
                 }
+
+                $bg: #f2f2f2;
+
+                &:nth-child(1) {
+                    background: $bg;
+                }
+
+                &:nth-child(2), &:nth-child(5) {
+                    background: darken($bg, 4%);
+                }
+
+                &:nth-child(3), &:nth-child(6), &:nth-child(9) {
+                    background: darken($bg, 4*2%);
+                }
+
+                &:nth-child(4), &:nth-child(7), &:nth-child(10) {
+                    background: darken($bg, 4*3%);
+                }
+
+                &:nth-child(8), &:nth-child(11), &:nth-child(13) {
+                    background: darken($bg, 4*4%);
+                }
+
+                &:nth-child(14) {
+                    background: darken($bg, 4*5%);
+                }
+
+                &:nth-child(12) {
+                    background: darken($bg, 4*6%);
+                }
+
             }
         }
     }
